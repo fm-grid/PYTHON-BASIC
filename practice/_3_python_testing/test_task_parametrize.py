ec04080287fd200d4a165fed06af1f7bb723f765
fm-grid <fmaciejak@griddynamics.com>
2024-05-31 16:36:25 +0200

    test and fix fibonacci

--- practice/_3_python_testing/test_task_parametrize.py
+++ practice/_3_python_testing/test_task_parametrize.py
@@ -9,6 +9,23 @@ Task:
  1. Write a test with @pytest.mark.parametrize decorator.
  2. Find the buggy function and fix it.
 """
+import pytest
+
+
+GROUND_TRUTH = [
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (7, 13),
+    (8, 21),
+    (9, 34),
+    (10, 55),
+    (11, 89),
+    (12, 144)
+]
 
 
 def fibonacci_1(n):
@@ -20,6 +37,20 @@ def fibonacci_1(n):
 
 def fibonacci_2(n):
     fibo = [0, 1]
-    for i in range(1, n+1):
-        fibo.append(fibo[i-1] + fibo[i-2])
+    for _ in range(1, n+1):
+        fibo.append(fibo[-1] + fibo[-2])
     return fibo[n]
+
+
+@pytest.mark.parametrize('test_input,test_output', GROUND_TRUTH)
+def test_fibonacci_1(test_input, test_output):
+    assert fibonacci_1(test_input) == test_output
+
+
+@pytest.mark.parametrize('test_input,test_output', GROUND_TRUTH)
+def test_fibonacci_2(test_input, test_output):
+    assert fibonacci_2(test_input) == test_output
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])

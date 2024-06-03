087789201f6172456068861bb1ebdd182e872ff2
fm-grid <fmaciejak@griddynamics.com>
2024-06-03 14:21:55 +0200

    finished task_1.py

--- practice/4_python_part_3/task_1.py
+++ practice/4_python_part_3/task_1.py
@@ -11,10 +11,23 @@ If entered date is from future, return negative value for number of days
     WrongFormatException
 """
 from datetime import datetime
+import pytest
+import re
+
+
+class WrongFormatException(Exception):
+    pass
 
 
 def calculate_days(from_date: str) -> int:
-    ...
+    match = re.match(r'(\d{4})-(\d{2})-(\d{2})', from_date)
+    if match == None:
+        raise WrongFormatException
+    year = int(match.group(1))
+    month = int(match.group(2))
+    day = int(match.group(3))
+    timedelta = datetime.now() - datetime(year, month, day)
+    return timedelta.days
 
 
 """
@@ -22,3 +35,21 @@ Write tests for calculate_days function
 Note that all tests should pass regardless of the day test was run
 Tip: for mocking datetime.now() use https://pypi.org/project/pytest-freezegun/
 """
+
+
+@pytest.mark.parametrize('test_input,test_output', [
+    ('2021-10-07', -1),
+    ('2021-10-05', 1)
+])
+@pytest.mark.freeze_time('2021-10-06')
+def test_correct_format(freezer, test_input, test_output):
+    assert calculate_days(test_input) == test_output
+
+
+@pytest.mark.parametrize('test_input', [
+    'test',
+    '01-02-2024'
+])
+def test_wrong_format(test_input):
+    with pytest.raises(WrongFormatException):
+        calculate_days(test_input)

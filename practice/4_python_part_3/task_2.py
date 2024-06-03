ecb3d93dd3248f269c1375f388c342616225f3c8
fm-grid <fmaciejak@griddynamics.com>
2024-06-03 12:33:06 +0200

    finished task_2.py

--- practice/4_python_part_3/task_2.py
+++ practice/4_python_part_3/task_2.py
@@ -10,12 +10,43 @@ Examples:
      11
 """
 import math
+import pytest
+
+
+class OperationNotFoundException(Exception):
+    pass
 
 
 def math_calculate(function: str, *args):
-    ...
+    if function not in math.__dict__:
+        raise OperationNotFoundException
+    if not callable(math.__dict__[function]):
+        raise OperationNotFoundException
+    function = math.__dict__[function]
+    return function(*args)
 
 
 """
 Write tests for math_calculate function
 """
+
+
+@pytest.mark.parametrize('test_input,test_output', [
+    (['ceil', 10.5], 11),
+    (['floor', 10.5], 10),
+    (['sin', 0], 0),
+    (['cos', 0], 1)
+])
+def test_good_operations(test_input, test_output):
+    assert math_calculate(*test_input) == test_output
+
+@pytest.mark.parametrize('test_input', [
+    ['this operation does not exist', 1, 2],
+    ['__name__', 1, 2],
+    ['inf', 1, 2],
+    ['e', 1, 2],
+    ['exp3', 1, 2]
+])
+def test_bad_operations(test_input):
+    with pytest.raises(OperationNotFoundException):
+        math_calculate(*test_input)

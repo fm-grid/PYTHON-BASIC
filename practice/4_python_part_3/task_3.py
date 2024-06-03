04503d299170e516a3625109b4663f0785881384
fm-grid <fmaciejak@griddynamics.com>
2024-06-03 14:44:42 +0200

    finished task_3.py

--- practice/4_python_part_3/task_3.py
+++ practice/4_python_part_3/task_3.py
@@ -10,12 +10,26 @@ Note that address may have several domain levels
     False
 """
 import re
+import pytest
 
 
 def is_http_domain(domain: str) -> bool:
-    ...
+    match = re.match(r'^(https?://)[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+/?$', domain)
+    return match is not None
 
 
 """
 write tests for is_http_domain function
 """
+
+
+@pytest.mark.parametrize('test_input,test_output', [
+    ('http://wikipedia.org', True),
+    ('https://ru.wikipedia.org/', True),
+    ('griddynamics.com', False),
+    ('?????', False),
+    ('https://....', False),
+    ('http:////////', False)
+])
+def test_is_http_domain(test_input, test_output):
+    assert is_http_domain(test_input) == test_output

564621bbe7d5a95efe9c4117e946a9453e3633b8
fm-grid <fmaciejak@griddynamics.com>
2024-06-06 14:07:57 +0200

    parsing city files

---
+++ practice/5_additional_topics/parsing_serialization_task/process_weather_data.py
@@ -0,0 +1,78 @@
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+import json
+from statistics import mean
+
+
+def define_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input', help='specify the directory containing folders for cities, if not specified the current working directory is used')
+    parser.add_argument('-o', '--output', help='specify the path and filename of the output file, if not specified the result is written to stdout')
+    return parser
+
+
+def parse_args(namespace: Namespace) -> tuple[Path, Path | None]:
+    input_dir = Path.cwd()
+    if namespace.input is not None:
+        input_dir = Path(namespace.input)
+    output_file = None
+    if namespace.output is not None:
+        output_file = Path(namespace.output)
+    return input_dir, output_file
+
+
+def read_cities_data(cities_dir: Path) -> dict:
+    data = {}
+    for city_folder in cities_dir.iterdir():
+        if not city_folder.is_dir():
+            continue
+        file_iter = city_folder.iterdir()
+        file = next(file_iter) # assume there is just one file
+        data[city_folder.name] = json.loads(file.read_text())
+    return data
+
+
+def parse_city(city_raw_dict: dict) -> dict:
+    temps = []
+    wind_speeds = []
+    for hour in city_raw_dict['hourly']:
+        temp = float(hour['temp'])
+        wind_speed = float(hour['wind_speed'])
+        temps.append(temp)
+        wind_speeds.append(wind_speed)
+    city_parsed_data = {
+        'mean_temp': round(mean(temps), 2),
+        'max_temp': max(temps),
+        'min_temp': min(temps),
+        'mean_wind_speed': round(mean(wind_speeds), 2),
+        'max_wind_speed': max(wind_speeds),
+        'min_wind_speed': min(wind_speeds)
+    }
+    return city_parsed_data
+
+
+def parse_cities(cities_raw_dict: dict) -> dict:
+    cities_parsed_data = {}
+    for city_name, city_dict in cities_raw_dict.items():
+        cities_parsed_data[city_name] = parse_city(city_dict)
+    return {
+        'mean_temp': mean([data['mean_temp'] for _, data in cities_parsed_data.items()]),
+        'mean_wind_speed': mean([data['mean_temp'] for _, data in cities_parsed_data.items()]),
+        'coldest_city': mean([data['mean_temp'] for _, data in cities_parsed_data.items()]),
+        'warmest_city': mean([data['mean_temp'] for _, data in cities_parsed_data.items()]),
+        'windiest_city': mean([data['mean_temp'] for _, data in cities_parsed_data.items()]),
+        'cities': cities_parsed_data
+    }
+
+
+def main() -> None:
+    parser = define_parser()
+    namespace = parser.parse_args()
+    input_dir, output_file = parse_args(namespace)
+    cities_raw_dict = read_cities_data(input_dir)
+    country_data = parse_cities(cities_raw_dict)
+    print(country_data)
+
+
+if __name__ == '__main__':
+    main()

9a3fb8d21a879f9472f7fa69d4dffe55f6b47dbb
fm-grid <fmaciejak@griddynamics.com>
2024-06-10 15:31:18 +0200

    first sheet

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -31,4 +31,123 @@ Links:
     - beautiful soup docs: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
     - lxml docs: https://lxml.de/
 """
+import requests
+from requests import Response
+from bs4 import BeautifulSoup
+import json
+from memoization import cached
 
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36'
+
+
+@cached
+def request(url: str) -> Response:
+    page = requests.get(url, headers={'User-Agent': USER_AGENT})
+    return page
+
+
+def scrape_main_table() -> list[dict]:
+    page = request('https://finance.yahoo.com/most-active')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    rows_data = []
+    trs = soup.find('table', class_='W(100%)').find('tbody').find_all('tr')
+    for tr in trs:
+        row_data = {}
+        tds = tr.find_all('td')
+        for td in tds:
+            field_name = td.get('aria-label')
+            row_data[field_name] = td.get_text()
+        rows_data.append(row_data)
+    return rows_data
+
+
+def scrape_ceo(symbol: str) -> dict:
+    page = request(f'https://finance.yahoo.com/quote/{symbol}/profile/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    table = soup.find('div', class_='table-container').find('table')
+    ths = table.find('thead').find('tr').find_all('th')
+    column_names = [th.get_text() for th in ths]
+    trs = table.find('tbody').find_all('tr')
+    rows_data = []
+    for tr in trs:
+        tds = tr.find_all('td')
+        row_data = {}
+        for index, td in enumerate(tds):
+            row_data[column_names[index]] = td.get_text()
+        rows_data.append(row_data)
+    ceos = list(filter(lambda row: 'CEO' in row['Title'], rows_data))
+    return ceos[0]
+
+
+def scrape_country(symbol: str) -> str:
+    page = request(f'https://finance.yahoo.com/quote/{symbol}/profile/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    div = soup.find('div', class_='address').find_all('div')[-1]
+    country = div.get_text()
+    return country
+
+
+def scrape_number_of_employees(symbol: str) -> int:
+    page = request(f'https://finance.yahoo.com/quote/{symbol}/profile/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    strong = soup.find('dl', class_='company-stats').find('strong')
+    if strong is None:
+        return 0
+    employees = int(strong.get_text().replace(',', ''))
+    return employees
+
+
+def render_row(widths: list[int], data: list[str]) -> str:
+    padded_data = [f' {s} ' for s in data]
+    aligned_data = [f'{s : <{w+2}}' for w, s in zip(widths, padded_data)]
+    return f'|{"|".join(aligned_data)}|'
+
+
+def print_table(title: str, columns: list[str], rows: list[list[str]]) -> None:
+    widths = []
+    for index, column in enumerate(columns):
+        max_width = max([len(row[index]) for row in rows] + [len(column)])
+        widths.append(max_width)
+    total_width = len(render_row(widths, columns))
+    print(f'{f'_{title.replace(' ', '_')}_' : ^{total_width}}'.replace(' ', '=').replace('_', ' '))
+    print(render_row(widths, columns))
+    for row in rows:
+        print('-' * total_width)
+        print(render_row(widths, row))
+    print('-' * total_width)
+
+
+def sheet1_rows() -> list[list[str]]:
+    rows = []
+    data = scrape_main_table()
+    for company in data:
+        symbol = company['Symbol']
+        ceo = scrape_ceo(symbol)
+        row = [
+            company['Name'],
+            symbol,
+            scrape_country(symbol),
+            str(scrape_number_of_employees(symbol)),
+            ceo['Name'],
+            ceo['Year Born']
+        ]
+        rows.append(row)
+    return rows
+
+
+def print_sheet1() -> None:
+    title = '5 stocks with most youngest CEOs'
+    columns = ['Name', 'Code', 'Country', 'Employees', 'CEO Name', 'CEO Year Born']
+    rows = sheet1_rows()
+    rows = sorted(rows, key=lambda row: row[-1], reverse=True)
+    rows = rows[:5]
+    print_table(title, columns, rows)
+
+
+def main():
+    print_sheet1()
+        
+
+if __name__ == '__main__':
+    main()

7229278fca875f56a26d5f249e9e1d4ec6f13df8
fm-grid <fmaciejak@griddynamics.com>
2024-06-11 14:59:52 +0200

    fixed bug where S&P was used every time

--- practice/6_web_scraping/scraper.py
+++ practice/6_web_scraping/scraper.py
@@ -85,7 +85,7 @@ def get_sheet1_data() -> list[list[str]]:
 def get_52_week_change(symbol: str) -> str:
     page = request(f'https://finance.yahoo.com/quote/{symbol}/key-statistics/')
     soup = BeautifulSoup(page.content, 'html.parser')
-    return soup.find('h3', text='Trading Information').find_parent().find_next_sibling().find('tbody').find_all('tr')[2].find_all('td')[1].get_text()
+    return soup.find('h3', text='Trading Information').find_parent().find_next_sibling().find('tbody').find_all('tr')[1].find_all('td')[1].get_text()
 
 
 def get_total_cash(symbol: str) -> str:

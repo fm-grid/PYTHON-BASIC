88a1971bb26842c214506a03128a546c2c74fc59
fm-grid <fmaciejak@griddynamics.com>
2024-06-12 14:04:48 +0200

    finished task1

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,9 +1,12 @@
+from concurrent.futures import ThreadPoolExecutor
 import os
 from random import randint
+import sys
 
 
 OUTPUT_DIR = './output'
 RESULT_FILE = './output/result.csv'
+sys.set_int_max_str_digits(0) # the default limit of 4300 is too small for ~fib(100_000)
 
 
 def fib(n: int):
@@ -16,11 +19,25 @@ def fib(n: int):
 
 
 def func1(array: list):
-    pass
+    array = list(set(array))
+    def func(number) -> None:
+        result = fib(number)
+        with open(f'{OUTPUT_DIR}/{number}.txt', 'w') as file:
+            file.write(str(result))
+    with ThreadPoolExecutor() as executor:
+        executor.map(func, array)
 
 
-def func2(result_file: str):
-    pass
+def func2(result_file: str = RESULT_FILE):
+    filenames = os.listdir(OUTPUT_DIR)
+    def func(filename: str) -> tuple[str, str]:
+        with open(f'{OUTPUT_DIR}/{filename}') as file:
+            value = file.read()
+            return (filename[:-4], value)
+    with ThreadPoolExecutor() as executor:
+        data = executor.map(func, filenames)
+        with open(result_file, 'x') as file:
+            file.write('\n'.join([f'{d[0]},{d[1]}' for d in data]))
 
 
 if __name__ == '__main__':
